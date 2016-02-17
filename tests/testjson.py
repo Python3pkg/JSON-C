822e457a9f60e269acafe3ab855c43f693176671
Ian Carvalho <iancarv@gmail.com>
2016-02-17 02:10:38 -0500

    defined tests

---
+++ tests/testjson.py
@@ -0,0 +1,40 @@
+import json
+
+JSON = '''
+	{
+		"Class1": {
+			"Decorator": "ClassDecorator",
+			"Superclass": "object",
+			"Properties": {
+				"Prop1": {
+					"Decorator": "PropertyDecorator",
+					"Type": "Int",
+					"Default": 0
+				},
+				"Prop2": {
+					"Decorator": "PropertyDecorator",
+					"Type": "String",
+					"Default": "Foo"
+				}
+			}
+		},
+		"Class2": {
+			"Decorator": "ClassDecorator",
+			"Superclass": "object",
+			"Properties": {
+				"Prop3": {
+					"Decorator": "PropertyDecorator",
+					"Type": "Int",
+					"Default": 0
+				},
+				"Prop4": {
+					"Decorator": "PropertyDecorator",
+					"Type": "String",
+					"Default": "Bar "
+				}
+			}
+		}
+	}
+'''
+
+obj = json.loads(JSON)
--- tests/tests.py
+++ tests/tests.py
@@ -6,3 +6,27 @@ class TestModelGeneration(unittest.TestCase):
 
 	def test_property(self):
 		pass
+
+class TestDefinitions(unittest.TestCase):
+	def test_assignment_definition(self):
+		pass
+
+	def test_property_definition(self):
+		pass
+
+	def test_getter_definition(self):
+		pass
+
+	def test_setter_definition(self):
+		pass
+
+	def test_args_definition(self):
+		pass
+
+	def test_function_definition(self):
+		pass
+
+
+class TestGenerators(unittest.TestCase):
+	def test_class_generation(self):
+		pass
