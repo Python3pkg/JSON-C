e322f61669b2cc28b3af21a9f5b696593dcbca19
Ian Carvalho <iancarv@gmail.com>
2016-02-17 15:35:06 -0500

    remove dupd duplicated PythPythonGenerator class

--- generators/definitions.py
+++ generators/definitions.py
@@ -5,24 +5,6 @@ def ident(s, nident=1):
 	ident = '\t' * nident
 	return ident + s.replace('\n', '\n' + ident)
 
-class PythonGenerator(object):
-	"""docstring for PythonGenerator"""
-	def __init__(self, cls):
-		super(PythonGenerator, self).__init__()
-		self.cls = cls
-		
-	def GenerateClass(self):
-		defs = PythonDefinitions()
-		content = ''
-		for i, p in enumerate(self.cls.properties):
-			print defs.Property(p.name, self.GenerateDefault(p.default), p.decorator)
-
-	def GenerateDefault(self, value):
-		return repr(value)
-
-
-
-
 class PythonDefinitions(object):
 	"""docstring for PythonDefinitions"""
 	def __init__(self):
